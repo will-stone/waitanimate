c3572064d3d22e471579bf3196b27cc3811cc78a
Will Stone <will@eggbox.io>
2016-06-08 19:00:27 +0100

    remove unused route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,11 +25,6 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      // .when('/about', {
-      //   templateUrl: 'views/about.html',
-      //   controller: 'AboutCtrl',
-      //   controllerAs: 'about'
-      // })
       .otherwise({
         redirectTo: '/'
       });
