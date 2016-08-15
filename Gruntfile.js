f1e70254945286d0ab3fde170bcf1507eb0d17d4
Will Stone <will@eggbox.io>
2016-08-15 14:17:26 +0100

    test remote

--- Gruntfile.js
+++ Gruntfile.js
@@ -445,7 +445,7 @@ module.exports = function (grunt) {
       },
       ghpages: {
         options: {
-          // remote: 'origin',
+          remote: 'git@github.com:eggboxio/waitanimate.git',
           branch: 'gh-pages',
           force: true
         }
