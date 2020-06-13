f44f1d410d1145a80bc6c2a93ce054832d780cb9
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-13 18:30:59 +0100

    Add rudimentary try catch around keyframes parser

--- src/utils/add-wait-to-keyframes.js
+++ src/utils/add-wait-to-keyframes.js
@@ -3,53 +3,57 @@ function roundToThreeDecimals(num) {
 }
 
 export function addWaitToKeyframes(keyframes, duration, waitTime) {
-  const asArray = keyframes
-    // Remove whitespace from start and end
-    .trim()
-    // Split by frame
-    .split('}')
-    // Remove empty array items
-    .filter(Boolean)
-    // Remove all whitespace from frame, including line breaks
-    .map((s) => s.replace(/\s/gu, ''))
-    // Put closing frame bracket back
-    .map((s) => s + '}')
-    // Split frame percentage and properties
-    .map((s) => ({
-      // Get number before opening frame and strip non-numeric
-      frame: Number(s.split('%')[0]),
-      properties: s.match(/\{(.*)\}/u)[1],
-    }))
+  try {
+    const asArray = keyframes
+      // Remove whitespace from start and end
+      .trim()
+      // Split by frame
+      .split('}')
+      // Remove empty array items
+      .filter(Boolean)
+      // Remove all whitespace from frame, including line breaks
+      .map((s) => s.replace(/\s/gu, ''))
+      // Put closing frame bracket back
+      .map((s) => s + '}')
+      // Split frame percentage and properties
+      .map((s) => ({
+        // Get number before opening frame and strip non-numeric
+        frame: Number(s.split('%')[0]),
+        properties: s.match(/\{(.*)\}/u)[1],
+      }))
 
-  return (
-    [
-      ...asArray.map((f) => ({
-        // Reduce frame percentages to accomodate wait and duration
-        frame: roundToThreeDecimals(
-          (f.frame * duration) / (duration + waitTime),
-        ),
-        properties: f.properties,
-      })),
-      // Extend last frame to end to simulate the wait time
-      {
-        frame: 100,
-        properties: asArray[asArray.length - 1].properties,
-      },
-    ]
-      // Turn back into string
-      .map((f) => {
-        const parsedProperties = f.properties
-          // Separate properties
-          .split(';')
-          // Add lines breaks between properties and indent
-          .join(';\n    ')
-          // Add space between property name and value
-          .replace(/:/gu, ': ')
-          // Remove line break added from the join
-          .trim()
-        return `  ${f.frame}% {\n    ${parsedProperties}\n  }`
-      })
-      // Add line break between frames
-      .join('\n')
-  )
+    return (
+      [
+        ...asArray.map((f) => ({
+          // Reduce frame percentages to accomodate wait and duration
+          frame: roundToThreeDecimals(
+            (f.frame * duration) / (duration + waitTime),
+          ),
+          properties: f.properties,
+        })),
+        // Extend last frame to end to simulate the wait time
+        {
+          frame: 100,
+          properties: asArray[asArray.length - 1].properties,
+        },
+      ]
+        // Turn back into string
+        .map((f) => {
+          const parsedProperties = f.properties
+            // Separate properties
+            .split(';')
+            // Add lines breaks between properties and indent
+            .join(';\n    ')
+            // Add space between property name and value
+            .replace(/:/gu, ': ')
+            // Remove line break added from the join
+            .trim()
+          return `  ${f.frame}% {\n    ${parsedProperties}\n  }`
+        })
+        // Add line break between frames
+        .join('\n')
+    )
+  } catch (error) {
+    return '  ERROR: Unable to parse keyframes, please try again.'
+  }
 }
